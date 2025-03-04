b141fb2e54bb52c72b22e2d9c6a2e2987f8859c7
Nick Wales <588472+nickwales@users.noreply.github.com>
2025-03-04 15:47:08 -0500

    Fixes cidr

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_security_group_rule" "group" {
   from_port         = each.value.port
   to_port           = each.value.port
   protocol          = "tcp"
-  cidr_blocks       = ["${each.value.node_address}/24"]
+  cidr_blocks       = ["${each.value.node_address}/32"]
   #cidr_blocks       = [lookup(each.value.meta, "source", "0.0.0.0/0")]
   description       = "security group rule added by CTS"
   security_group_id = var.security_group_id
